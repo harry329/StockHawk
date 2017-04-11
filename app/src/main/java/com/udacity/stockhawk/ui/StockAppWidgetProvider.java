e5d70b18240831698dc20727c1acfc7ddef577ec
harry <harsimrans329@gmail.com>
2017-04-10 22:09:07 -0500

    App Widget part-1.

---
+++ app/src/main/java/com/udacity/stockhawk/ui/StockAppWidgetProvider.java
@@ -0,0 +1,12 @@
+package com.udacity.stockhawk.ui;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by harry on 4/10/17.
+ */
+
+public class StockAppWidgetProvider extends AppWidgetProvider {
+
+
+}
